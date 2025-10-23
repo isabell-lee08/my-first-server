c8bff2e3fb1761b523461456743dda41149408f3
isabell-lee08 <isabellleee8@gmail.com>
2025-10-23 17:32:17 -0400

    server.js demo

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Your server code goes here. Host your server on localhost:3000
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, response) {
+    if (request.url === '/' || request.url === '/index.html') {
+        fs.readFile('index.html', function(error, data) {
+            if (error) {
+                response.writeHead(500);
+                response.end('Something went wrong on our end.');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
+            }
+        })
+    } else {
+        response.writeHead(404);
+        response.end('Page not found');
+    }
+})
+
+server.listen(3000, function() {
+    console.log('Server is running at localhost:3000');
+})
